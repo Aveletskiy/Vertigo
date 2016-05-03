60c5a5445f880367c031e6f82bc6d49aed1898cb
Aveletsky <ixus.van.axel@gmail.com>
2016-05-04 01:28:28 +0300

    Week work

---
+++ Vertigo/Coords.xaml.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Шаблон элемента пустой страницы задокументирован по адресу http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Vertigo
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class Coords : Page
+    {
+        private Geolocator _geolocator { get; set; }
+        private Geoposition Geoposition { get; set; }
+        private MapIcon MapIcon { get; set; }
+
+        //// A pointer to the main page
+        private MainPage _rootPage = MainPage.Current;
+
+        public Coords()
+        {
+            this.InitializeComponent();
+
+
+
+            //            _geolocator = new Geolocator() { ReportInterval = 1000 };
+            //          _geolocator.DesiredAccuracy = PositionAccuracy.High;
+            //            _geolocator.StatusChanged += Geolocator_StatusChanged;
+            //          _geolocator.PositionChanged += Geolocator_PositionChanged;
+
+
+            mapControl.MapServiceToken = "nD18JaojG92g4lqLyhzI~wo1ajtwOwbPAoUAUUxu6Sg~ArEdm5wChACupRSXm50wPw8v7drL6dNMRMWztobTJOj1rrcNoh0uIch5I_XHnOvp";
+            zoomLevel.Minimum = mapControl.MinZoomLevel;
+            zoomLevel.Maximum = mapControl.MaxZoomLevel;
+            mapControl.ZoomLevel = 17;
+            UpdateLocationData();
+        }
+
+        private void TrackingInit(object sender, RoutedEventArgs e)
+        {
+            if ((bool)TrackingButton.IsChecked) StartTracking(sender, e); else StopTracking(sender, e);
+        }
+
+        private async void Geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                if (Geoposition == null || args.Position.Coordinate.Accuracy <= Geoposition.Coordinate.Accuracy)
+                {
+                    Geoposition = args.Position;
+                    if (MapIcon != null)
+                    {
+                        RemoveMapIcon(MapIcon);
+                    }
+                    MapIcon = AddGeopoint(args.Position.Coordinate.Point, "Вы");
+                    //mapControl.Center = Geoposition.Coordinate.Point;
+                    UpdateLocationData();
+                }
+            });
+        }
+
+
+        private async void StartTracking(object sender, RoutedEventArgs e)
+        {
+            // Request permission to access location
+            var accessStatus = await Geolocator.RequestAccessAsync();
+
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+                    // You should set MovementThreshold for distance-based tracking
+                    // or ReportInterval for periodic-based tracking before adding event
+                    // handlers. If none is set, a ReportInterval of 1 second is used
+                    // as a default and a position will be returned every 1 second.
+                    //
+                    // Value of 2000 milliseconds (2 seconds) 
+                    // isn't a requirement, it is just an example.
+                    _geolocator = new Geolocator { ReportInterval = 1000 };
+
+                    // Subscribe to PositionChanged event to get updated tracking positions
+                    _geolocator.PositionChanged += Geolocator_PositionChanged;
+
+
+                    // Subscribe to StatusChanged event to get updates of location status changes
+                    _geolocator.StatusChanged += Geolocator_StatusChanged;
+
+                    _rootPage.NotifyUser("Waiting for update...", MainPage.NotifyType.StatusMessage);
+                    LocationDisabledMessage.Visibility = Visibility.Collapsed;
+                    //TrackingButton.IsChecked = true;
+
+                    break;
+
+                case GeolocationAccessStatus.Denied:
+                    _rootPage.NotifyUser("Access to location is denied.", MainPage.NotifyType.ErrorMessage);
+                    LocationDisabledMessage.Visibility = Visibility.Visible;
+                    break;
+
+                case GeolocationAccessStatus.Unspecified:
+                    _rootPage.NotifyUser("Unspecificed error!", MainPage.NotifyType.ErrorMessage);
+                    LocationDisabledMessage.Visibility = Visibility.Collapsed;
+                    break;
+            }
+        }
+
+        private void StopTracking(object sender, RoutedEventArgs e)
+        {
+            _geolocator.PositionChanged -= Geolocator_PositionChanged;
+            _geolocator.StatusChanged -= Geolocator_StatusChanged;
+            _geolocator.DesiredAccuracy = PositionAccuracy.High;
+            _geolocator = null;
+
+            //TrackingButton.IsEnabled = false;
+
+            // Clear status
+            _rootPage.NotifyUser("Tracking stoped", MainPage.NotifyType.StatusMessage);
+        }
+
+        async private void Geolocator_StatusChanged(Geolocator sender, StatusChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // Show the location setting message only if status is disabled.
+                LocationDisabledMessage.Visibility = Visibility.Collapsed;
+
+                switch (args.Status)
+                {
+                    case PositionStatus.Ready:
+                        // Location platform is providing valid data.
+                        _rootPage.RewriteStatus (args);
+                        _rootPage.NotifyUser("Location platform is ready.", MainPage.NotifyType.StatusMessage);
+                        break;
+
+                    case PositionStatus.Initializing:
+                        // Location platform is attempting to acquire a fix. 
+                        _rootPage.RewriteStatus(args);
+                        _rootPage.NotifyUser("Location platform is attempting to obtain a position.", MainPage.NotifyType.StatusMessage);
+                        break;
+
+                    case PositionStatus.NoData:
+                        // Location platform could not obtain location data.
+                        _rootPage.RewriteStatus(args);
+                        _rootPage.NotifyUser("Not able to determine the location.", MainPage.NotifyType.ErrorMessage);
+                        break;
+
+                    case PositionStatus.Disabled:
+                        // The permission to access location data is denied by the user or other policies.
+                        _rootPage.RewriteStatus(args);
+                        _rootPage.NotifyUser("Access to location is denied.", MainPage.NotifyType.ErrorMessage);
+
+                        // Show message to the user to go to location settings
+                        LocationDisabledMessage.Visibility = Visibility.Visible;
+
+
+
+                        // Clear cached location data if any
+                        //UpdateLocationData(null);
+                        break;
+
+                    case PositionStatus.NotInitialized:
+                        // The location platform is not initialized. This indicates that the application 
+                        // has not made a request for location data.
+                        _rootPage.RewriteStatus(args);
+                        _rootPage.NotifyUser("No request for location is made yet.", MainPage.NotifyType.StatusMessage);
+                        break;
+
+                    case PositionStatus.NotAvailable:
+                        // The location platform is not available on this version of the OS.
+                        _rootPage.RewriteStatus(args);
+                        _rootPage.NotifyUser("Location is not available on this version of the OS.", MainPage.NotifyType.ErrorMessage);
+                        break;
+
+                    default:
+                        _rootPage.RewriteStatus(args);
+                        _rootPage.NotifyUser(string.Empty, MainPage.NotifyType.StatusMessage);
+                        break;
+                }
+            });
+        }
+
+        public void RemoveMapIcon(MapIcon mapIcon)
+        {
+            mapControl.MapElements.Remove(mapIcon);
+        }
+
+        public void UpdateLocationData()
+        {
+            if (_geolocator != null && Geoposition!=null)
+            {
+               if (!(bool)CenterPin.IsChecked)
+                    mapControl.Center = Geoposition.Coordinate.Point;
+                mapControl.LandmarksVisible = true;
+            }
+        }
+
+        public MapIcon AddGeopoint(Geopoint geopoint, string name)
+        {
+            MapIcon mapIcon = new MapIcon();
+            mapIcon.Location = geopoint;
+            mapIcon.NormalizedAnchorPoint = new Point(0.5, 1.0);
+            mapIcon.Title = name;
+            mapIcon.ZIndex = 0;
+            mapControl.MapElements.Add(mapIcon);
+            mapIcon.Visible = true;
+            return mapIcon;
+        }
+
+        private void ZoomLevelHendler(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            mapControl.ZoomLevel = zoomLevel.Value;
+        }
+
+        private void ZoomLevelChanged(MapControl sender, object args)
+        {
+            zoomLevel.Value = mapControl.ZoomLevel;
+        }
+
+        private void ClickAdd(object sender, RoutedEventArgs e)
+        {
+            zoomLevel.Value++;
+        }
+
+        private void ClickMinus(object sender, RoutedEventArgs e)
+        {
+            zoomLevel.Value--;
+        }
+
+        private void KeyDownPin(object sender, KeyRoutedEventArgs e)
+        {
+
+        }
+    }
+
+   
+}
---
+++ Vertigo/MainPage.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using Windows.Devices.Geolocation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Vertigo
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Geolocator Geolocator { get; set; }
+        private Geoposition Geoposition { get; set; }
+        public static MainPage Current;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Current = this;
+            NotifyUser(String.Empty, NotifyType.StatusMessage);
+            frameMain.Navigate(typeof(Coords));
+            textBlockTitle.Text = "Координаты";
+            Geolocator = new Geolocator() { ReportInterval = 1000 };
+            Geolocator.DesiredAccuracy = PositionAccuracy.High;
+            Geolocator.StatusChanged += Geolocator_StatusChanged;
+            Geolocator.PositionChanged += Geolocator_PositionChanged;
+        }
+
+        private async void Geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                if (Geoposition == null || args.Position.Coordinate.Accuracy <= Geoposition.Coordinate.Accuracy)
+                {
+                    Geoposition = args.Position;
+                    TextBlockCoords.Text = Geoposition.Coordinate.Latitude.ToString() + ' ' 
+                    + Geoposition.Coordinate.Longitude.ToString()+':'+ Geoposition.Coordinate.Accuracy.ToString();
+                }
+                
+            });
+        }
+
+        internal void RewriteStatus(StatusChangedEventArgs args)
+        {
+            switch (args.Status)
+            {
+                case PositionStatus.Ready:
+                    // Location platform is providing valid data.
+                    StatusLabel.Text = "Status: Ready";
+                    break;
+
+                case PositionStatus.Initializing:
+                    // Location platform is attempting to acquire a fix. 
+                    StatusLabel.Text = "Status: Initializing";
+                    break;
+
+                case PositionStatus.NoData:
+                    // Location platform could not obtain location data.
+                    StatusLabel.Text = "Status: No data";
+                    break;
+
+                case PositionStatus.Disabled:
+                    // The permission to access location data is denied by the user or other policies.
+                    StatusLabel.Text = "Status: Disabled";
+                    break;
+
+                case PositionStatus.NotInitialized:
+                    // The location platform is not initialized. This indicates that the application 
+                    // has not made a request for location data.
+                    StatusLabel.Text = "Status: Not initialized";
+                    break;
+
+                case PositionStatus.NotAvailable:
+                    // The location platform is not available on this version of the OS.
+                    StatusLabel.Text = "Status: Not available";
+                    break;
+
+                default:
+                    StatusLabel.Text = "Status: Unknown";
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Event handler for StatusChanged events. It is raised when the 
+        /// location status in the system changes.
+        /// </summary>
+        /// <param name="sender">Geolocator instance</param>
+        /// <param name="args">Statu data</param>
+        async private void Geolocator_StatusChanged(Geolocator sender, StatusChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // Show the location setting message only if status is disabled.
+
+
+            });
+        }
+
+
+
+
+
+         private void OnStatusChanged(Geolocator sender, StatusChangedEventArgs e)
+        {
+            
+        }
+
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Messages.IsSelected)
+            {
+                frameMain.Navigate(typeof(Messages));
+                textBlockTitle.Text = "Сообщения";
+            }
+            else if (Coords.IsSelected)
+            {
+                frameMain.Navigate(typeof(Coords));
+                textBlockTitle.Text = "Координаты";
+            }
+            else if (Payment.IsSelected)
+            {
+                frameMain.Navigate(typeof(Payment));
+                textBlockTitle.Text = "Оплата";
+            }
+            else if (Settings.IsSelected)
+            {
+                frameMain.Navigate(typeof(Settings));
+                textBlockTitle.Text = "Настройки";
+            }
+
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            splitView.IsPaneOpen = !splitView.IsPaneOpen;
+        }
+        private string GetStatusString(PositionStatus status)
+        {
+            var strStatus = "";
+
+            switch (status)
+            {
+                case PositionStatus.Ready:
+                    strStatus = "Location is available.";
+                    break;
+
+                case PositionStatus.Initializing:
+                    strStatus = "Geolocation service is initializing.";
+                    break;
+
+                case PositionStatus.NoData:
+                    strStatus = "Location service data is not available.";
+                    break;
+
+                case PositionStatus.Disabled:
+                    strStatus = "Location services are disabled. Use the " +
+                                "Settings charm to enable them.";
+                    break;
+
+                case PositionStatus.NotInitialized:
+                    strStatus = "Location status is not initialized because " +
+                                "the app has not yet requested location data.";
+                    break;
+
+                case PositionStatus.NotAvailable:
+                    strStatus = "Location services are not supported on your system.";
+                    break;
+
+                default:
+                    strStatus = "Unknown PositionStatus value.";
+                    break;
+            }
+
+            return (strStatus);
+
+        }
+
+
+        public enum NotifyType
+        {
+            StatusMessage,
+            ErrorMessage
+        };
+
+        /// <summary>
+        /// Used to display messages to the user
+        /// </summary>
+        /// <param name="strMessage"></param>
+        /// <param name="type"></param>
+        public void NotifyUser(string strMessage, NotifyType type)
+        {
+            switch (type)
+            {
+                case NotifyType.StatusMessage:
+                    StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+                    break;
+                case NotifyType.ErrorMessage:
+                    StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                    break;
+            }
+            StatusBlock.Text = strMessage;
+
+            // Collapse the StatusBlock if it has no text to conserve real estate.
+            StatusBorder.Visibility = (StatusBlock.Text != String.Empty) ? Visibility.Visible : Visibility.Collapsed;
+            if (StatusBlock.Text != String.Empty)
+            {
+                StatusBorder.Visibility = Visibility.Visible;
+                StatusPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                StatusBorder.Visibility = Visibility.Collapsed;
+                StatusPanel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
+    }
+}
